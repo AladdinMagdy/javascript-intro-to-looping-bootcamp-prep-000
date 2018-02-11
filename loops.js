7932f5860069f6349c7d18523e3105529e159b19
Aladdin Magdy <aladdinmagdysaber167@commerce.helwan.edu.eg>
2018-02-11 03:40:56 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 function forLoop(array){
   for(let i = 0; i < 27; i++){
-    if(i === 1 || i === 0){
+    if(i === 1){
     array[i] = `I am ${i} strange loop.`;
     }else{
-      array[i] = `I am ${i} strange loops.`;
+      array[i] = `I am ${i} strange loop${i === 0 ? '' : 's'}.`;
     }
   }
   return array;
